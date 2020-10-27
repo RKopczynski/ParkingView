b5c3aab8e714b560056ae8301c85b87a24ac5dfd
Rafal Kopczynski <rafal.kopczynski1995@gmail.com>
2020-10-27 22:06:36 +0100

    Initial commit.

---
+++ Web/Pages/FetchData.razor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using RafEla.ParkingView.Shared;
+
+namespace RafEla.ParkingView.Web.Pages {
+
+    public partial class FetchData
+    {
+        private WeatherForecast[] forecasts;
+        
+        [Inject] public HttpClient Http { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            forecasts = await Http.GetFromJsonAsync<WeatherForecast[]>("https://localhost:5001/weatherforecast");
+        }
+    }
+}
