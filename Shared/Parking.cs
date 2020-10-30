02344db82c65e2d7200e4406413a6fe522da3b56
EKaczmarek <e.kaczmarek01@gmail.com>
2020-10-30 19:57:06 +0100

    Adding fields to parking class

---
+++ Shared/Parking.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace RafEla.ParkingView.Shared
+{
+    public class Parking
+    {
+        public int ParkingId { get; set;}
+
+        public string Street { get; set; }
+
+        public int StreetNo { get; set; }
+
+        public string Postcode { get; set;}
+
+        public string City { get; set; }
+    }
+}
