173c34ecc4213739317b488b92257b1c43f5d408
EKaczmarek <e.kaczmarek01@gmail.com>
2021-03-22 14:24:53 +0100

    Split error

--- Server/Data/LIstExtensions.cs
+++ Server/Data/LIstExtensions.cs
@@ -12,9 +12,10 @@ namespace RafEla.ParkingView.Server.Data
             var data = new List<ParsedLine>();
             var items = str.Split("\n");
             var header = "Czas_Rejestracji";
+            String [] delimiters = {";", "\""};
             foreach (var item in items)
             {
-                var fields = item.Split(";");
+                var fields = item.Split(delimiters, StringSplitOptions.None);
                 if (fields[0] != header && fields[0] != "")
                     {
                         data.Add(new (DateTime.Parse(fields[0]), int.Parse(fields[1]), fields[4]));
